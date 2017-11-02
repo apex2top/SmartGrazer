f6488714d84e9ccff85fd386a0427bc8a988d79e
aborgardt <aborgardt@ac24ebb7-9227-423d-8e2a-3b3772374dbc>
2017-11-02 11:33:28 +0000

    - Testruns - Added new payload grammars
    git-svn-id: https://svn.aborgardt.com/private/Projekte/SmartGrazer/trunk@404 ac24ebb7-9227-423d-8e2a-3b3772374dbc

--- TestRunner.cmd
+++ TestRunner.cmd
@@ -3,25 +3,24 @@
 set mypath=%cd%
 
 set timelimit=5 10 15
-set generator=dharma smarty
+set generator=dharma
 set level=0 1
 set repeat=50
 
 
 for /L %%c in (1,1,%repeat%) do (
 	for %%t in (%timelimit%) do (
-		for %%g in (%generator%) do (
-			for %%l in (%level%) do (
-				set dir=%mypath%\testrun\%%g\%%t\aborgardt.bwapp\
-				set command=python SmartGrazer.py -c -x aborgardt.bwapp\level-%%l.json --overwrite smartgrazer.imps.smithy.generator=%%g smartgrazer.imps.webber.timelimit=%%t
-				echo "Command: %command% > %dir%level-%%l-%%c.json"
-				
-				if not exist "%dir%" (
-					md "%dir%"
-				)
-				
-				%command% > %dir%level-%%l-%%c.json 2>&1
+	
+		for %%l in (%level%) do (
+			set dir=%mypath%\testrun\aborgardt.bwapp\
+			set command=python SmartGrazer.py -c -x aborgardt.bwapp\level-%%l.json --overwrite smartgrazer.imps.smithy.generator=%generator% smartgrazer.imps.webber.timelimit=%%t
+			echo "Command: %command% > %dir%level-%%l-%%c.json"
+			
+			if not exist "%dir%" (
+				md "%dir%"
 			)
+			
+			%command% > %dir%level-%%l-%%t-%%c.json 2>&1
 		)
 	)
 )
