ec661629f379a1fe04111f6af714c9c179b603bb
Alexander Borgardt <aborgardt.public@gmail.com>
2018-08-28 10:31:41 +0200

    Basic loop

---
+++ SmartGrazer.py
@@ -0,0 +1,20 @@
+class SmartGrazer(object):
+    def run(self):
+        # load configuration
+
+        tries = 10000
+        found = False
+        while tries > 0 and not found:
+            # attack
+
+            # analyse
+
+            # adjust
+
+            tries = tries - 1
+        pass
+
+
+if __name__ == '__main__':
+    smartgrazer = SmartGrazer()
+    smartgrazer.run()
