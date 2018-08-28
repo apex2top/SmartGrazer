97239866574b5cc014a5923fb17ac2d760d1afdd
Alexander Borgardt <aborgardt.public@gmail.com>
2018-08-28 10:22:52 +0200

    Starting over.

--- doc/thesis/source/clean.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set FILE_LIST=(*.brf *.alg *.acr *.acn *.lof *.lol *.lot *.spl *.run.xml *.bcf *.ind *.ilg *.glo *.idx *.synctex.gz *.toc *.snm *.aux *.out *.nav *.log *.bbl *.blg *.glsdefs *.ist *.glg *.gls)
-
-for %%i in %FILE_LIST% do DEL /F %%i
--- imps/annelysa/AnalyzerPlugin.py
+++
@@ -1,5 +0,0 @@
-class AnalyzerPlugin(object):
-    def __init__(self, root, tree, needle):
-        self._root = root
-        self._tree = tree
-        self._needle = needle
